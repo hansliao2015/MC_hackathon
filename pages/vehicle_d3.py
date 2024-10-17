3bbe2fb3cbc01c1b262d47aa5fa3c2b0c9aefac1
hansliao2015 <hansliao2015@gmail.com>
2024-10-17 13:08:37 +0800

    Refactor file paths for vehicle HTML and JavaScript files

--- pages/vehicle_d3.py
+++ pages/vehicle_d3.py
@@ -2,9 +2,9 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 if __name__ == "__main__":
-  with open("./index.html") as fp:
+  with open("./html/vehicle.html") as fp:
     html = fp.read()
-  with open("./scripts/vehicle.js") as fp:
+  with open("./js/vehicle.js") as fp:
     script = fp.read()
     html = html.replace("SCRIPT", f"<script>{script}</script>")
 
