a743f0969f8d3b739681be87da9b4396a8fd6064
hansliao2015 <hansliao2015@gmail.com>
2024-10-17 20:05:52 +0800

    Refactor file path for vehicle HTML file

--- pages/vehicle.py
+++ pages/vehicle.py
@@ -2,7 +2,7 @@ import streamlit as st
 import streamlit.components.v1 as components
 
 def drawPlot():
-  with open("./html/vehicle-v2.html") as fp:
+  with open("./html/vehicle.html") as fp:
     html = fp.read()
 
   components.html(html, height=1000, width=820)
